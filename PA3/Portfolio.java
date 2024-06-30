ec13de002902ff469ba4749930c7bb4cf633b5e1
Tianfang Guo <taylorguo123@gmail.com>
2024-06-29 23:44:43 -0500

    wrote out all portfolio methods

--- PA3/Portfolio.java
+++ PA3/Portfolio.java
@@ -1,27 +1,64 @@
 public class Portfolio{
     
     //attributes
-    BankAccount checkings;
+    BankAccount checking;
 
     BankAccount savings;
 
-    
+    //constructor
+    public Portfolio(){
+        checking = new BankAccount();
+        savings = new BankAccount();
+    }
+
     //methods
     public void deposit(double amount, String account){
-    
+        if(account == "C"){
+            this.checking.deposit(amount);        
+        }  
+        else if(account == "S"){
+            this.savings.deposit(amount);
+        }
+        else{
+            System.out.println("invalid account detected, must pass in 'S' or 'C'");
+        }
     }
 
     public void withdraw(double amount, String account){
-
+        if(account == "C"){
+            this.checking.withdraw(amount);
+        }  
+        else if(account == "S"){
+            this.savings.withdraw(amount);
+        }
+        else{
+            System.out.println("invalid account detected, must pass in 'S' or 'C'");
+        }    
     }
 
     public void transfer(double amount, String account){
-
+        if(account == "C"){
+            this.checking.withdraw(amount);
+            this.savings.deposit(amount);
+        }  
+        else if(account == "S"){
+            this.savings.withdraw(amount);
+            this.checking.deposit(amount);
+        }
+        else{
+            System.out.println("invalid account detected, must pass in 'S' or 'C'");
+        }        
     }
 
     public double getBalance(String account){
-    return 0;
+        if(account == "C"){
+            return this.checking.getBalance();
+        }
+        else if(account == "S"){
+            return this.savings.getBalance();
+        }   
+        else{
+            System.out.println("invalid account detected, must pass in 'S' or 'C'");
+        }
     }
-    
-
 }
