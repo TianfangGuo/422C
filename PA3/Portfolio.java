35d76696188045bf51d211e10dc72f71b680cb62
Tianfang Guo <taylorguo123@gmail.com>
2024-06-29 23:10:56 -0500

    initial skeleton files for PA3

---
+++ PA3/Portfolio.java
@@ -0,0 +1,27 @@
+public class Portfolio{
+    
+    //attributes
+    BankAccount checkings;
+
+    BankAccount savings;
+
+    
+    //methods
+    public void deposit(double amount, String account){
+    
+    }
+
+    public void withdraw(double amount, String account){
+
+    }
+
+    public void transfer(double amount, String account){
+
+    }
+
+    public double getBalance(String account){
+    return 0;
+    }
+    
+
+}
