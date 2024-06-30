f2255484e3bab46740cd9527cdf999e2f04e18e2
Tianfang Guo <taylorguo123@gmail.com>
2024-06-29 23:45:35 -0500

    wrote out all Portfolio Methods

---
+++ PA3/PortfolioTester.java
@@ -0,0 +1,5 @@
+public class PortfolioTester{
+    Portfolio test = new Portfolio();
+    System.out.printf("%.2f%n", test.checking.getBalance());
+    //System.out.printf("%f%n", test.checking.getBalance());
+}
