0eb7f67756dbcf39298dd34933d79664f494fe30
Tianfang Guo <taylorguo123@gmail.com>
2024-06-30 16:52:10 -0500

    finished preliminary debugging of methods

--- PA3/PortfolioTester.java
+++ PA3/PortfolioTester.java
@@ -1,5 +1,32 @@
 public class PortfolioTester{
-    Portfolio test = new Portfolio();
-    System.out.printf("%.2f%n", test.checking.getBalance());
-    //System.out.printf("%f%n", test.checking.getBalance());
+
+    public static void main(String[] args){
+        Portfolio test = new Portfolio();
+        System.out.printf("%f\n", test.getBalance("S"));
+        System.out.printf("%f\n", test.getBalance("C"));
+        System.out.printf("%f\n", test.getBalance("badinput"));
+
+        test.deposit(69, "C");
+        test.deposit(420, "S");
+        test.deposit(1337, "badinput");
+        System.out.printf("%f\n", test.getBalance("S"));
+        System.out.printf("%f\n", test.getBalance("C"));
+        System.out.printf("%f\n", test.getBalance("badinput"));
+
+        test.withdraw(1, "C");
+        test.withdraw(2, "S");
+        test.withdraw(3, "badinput");
+        System.out.printf("%f\n", test.getBalance("S"));
+        System.out.printf("%f\n", test.getBalance("C"));
+        System.out.printf("%f\n", test.getBalance("badinput"));
+
+        test.transfer(30, "C");
+        test.transfer(20, "S");
+        test.transfer(10, "badinput");
+        System.out.printf("%f\n", test.getBalance("S"));
+        System.out.printf("%f\n", test.getBalance("C"));
+        System.out.printf("%f\n", test.getBalance("badinput"));
+
+
+    }
 }
