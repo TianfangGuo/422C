1aa8c364fdd5e647767206674b48ae6b367d6d70
Tianfang <tguo@tfgmini>
2024-07-25 15:14:06 -0500

    finished

--- PA4/Bank.java
+++ PA4/Bank.java
@@ -1,27 +1,49 @@
 class Bank
 {
     public Bank(){
-        accounts = new int[10];
-        for(int i : accounts){
-            i = 10000;
+        this.accounts = new int[10];
+        for(int i = 0; i < 10; i++){
+            accounts[i] = 10000;
+            System.out.println("initialized account " + i + " to " + accounts[i]);
         }   
-        total = 100000;
+        this.total = 100000;
+        System.out.println("initial audit is $100,000");
+        this.audit_signal = 0;
+    }
+    public void print_balances(){
+        for(int i = 0; i < 10; i++){
+            System.out.println(accounts[i]);
+        }   
     }
     public void Transfer(int from, int to, int amount){
-        accounts[from] -= amount;
-        Thread.sleep(10);
-        account[to] += amount;
-        audit();
+        System.out.println("transfering " + amount + " from account " + from + " to account " + to);
+        this.accounts[from] -= amount;
+
+        try {
+            Thread.sleep (100);
+        } catch (InterruptedException e) {
+            //...
+        }
+        
+        this.accounts[to] += amount;
+            if(this.audit_signal%78 == 0){
+                audit();
+                this.audit_signal += 7;
+            }
     }
-    public void audit(){
+    public int audit(){
         int sum = 0;
         for(int i : accounts){
             sum += i;
         }
-        total = sum;
+        this.total = sum;
+        System.out.println("========================audit total is " + total + "========================");
+
+        return sum;
     }
     
-    private int[] accounts;
-    private int total;
+    public int[] accounts;
+    public int total;
+    public int audit_signal;
             
 }
--- PA4/TransferThread.java
+++ PA4/TransferThread.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 class TransferThread extends Thread
 {
     public TransferThread(Bank b, int from, int max){
@@ -6,7 +8,43 @@ class TransferThread extends Thread
         maxAmount = max;
     }
     public void run(){
+        int audit_signal = 0;
 
+        for(int i = 0; i < 25; i++){
+            Random r = new Random();
+            bank.audit_signal++;
+            bank.Transfer(fromAccount, r.nextInt(10), maxAmount);
+            // audit_signal++;
+            // if(audit_signal == 5){
+            //     audit_signal = 0;
+            //     bank.audit();
+            // }
+        }
+    }
+    public static void main(String[] args){
+        Bank b = new Bank();
+        Thread bank1 = new TransferThread(b, 0, 422);
+        Thread bank2 = new TransferThread(b, 1, 145);
+        Thread bank3 = new TransferThread(b, 2, 562);
+        Thread bank4 = new TransferThread(b, 3, 28);
+        Thread bank5 = new TransferThread(b, 4, 146);
+        Thread bank6 = new TransferThread(b, 5, 66);
+        Thread bank7 = new TransferThread(b, 6, 7);
+        Thread bank8 = new TransferThread(b, 7, 290);
+        Thread bank9 = new TransferThread(b, 8, 567);
+        Thread bank10 = new TransferThread(b, 9, 12);
+
+        bank1.start();
+        bank2.start();
+        bank3.start();
+        bank4.start();
+        bank5.start();
+        bank6.start();
+        bank7.start();
+        bank8.start();
+        bank9.start();
+        bank10.start();
+        
     }
 
     private Bank bank;
